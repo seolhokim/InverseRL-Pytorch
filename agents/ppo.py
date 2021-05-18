e80f6f69edb0c9c37e8cd88d55cc129290860b97
seolhokim <kilmya1@naver.com>
2021-05-18 17:46:16 +0900

    refactor ReplayBuffer

--- agents/ppo.py
+++ agents/ppo.py
@@ -6,15 +6,16 @@ import torch.optim as optim
 import numpy as np
 
 from networks.agent_network import Actor, Critic
-from utils.utils import Rollouts
+from utils.utils import ReplayBuffer, make_mini_batch, make_one_mini_batch, convert_to_tensor
 
 class PPO(nn.Module):
     def __init__(self,writer,device,state_dim, action_dim, args, demonstrations_location_args): 
         super(PPO, self).__init__()
         self.writer = writer
         self.device = device
-        self.data = Rollouts()
         self.args = args
+        
+        self.data = ReplayBuffer(action_prob_exist = True, max_size = self.args.traj_length, state_dim = state_dim, num_action = action_dim)
         file_size = 120
         
         f = open(demonstrations_location_args.expert_state_location,'rb')
@@ -34,29 +35,29 @@ class PPO(nn.Module):
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=args.actor_lr)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=args.critic_lr)
         
-    def pi(self,x):
+    def get_action(self,x):
         return self.actor(x)
     
     def v(self,x):
         return self.critic(x)
     
     def put_data(self,transition):
-        self.data.append(transition)
+        self.data.put_data(transition)
     
     
     def train(self, writer, discriminator, discriminator_batch_size, state_rms, n_epi, airl = False):
-        s_, a_, r_, s_prime_, done_mask_, old_log_prob_ = self.data.make_batch(self.device)
-        
+        data = self.data.sample(shuffle = False)
+        states, actions, rewards, next_states, done_masks, old_log_probs = convert_to_tensor(self.device, data['state'], data['action'], data['reward'], data['next_state'], data['done'], data['log_prob'])
 
         if airl == False:
-            agent_s,agent_a = self.data.choose_s_a_mini_batch(discriminator_batch_size, s_, a_)
-            expert_s,expert_a = self.data.choose_s_a_mini_batch(discriminator_batch_size, self.expert_states, self.expert_actions)
+            agent_s,agent_a = make_one_mini_batch(discriminator_batch_size, states, actions)
+            expert_s,expert_a = make_one_mini_batch(discriminator_batch_size, self.expert_states, self.expert_actions)
             
             expert_s = np.clip((expert_s - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
             self.train_discriminator(writer, discriminator, n_epi, agent_s, agent_a, expert_s, expert_a)
         else:
-            agent_s,agent_a,agent_next_s,agent_done_mask = self.data.choose_s_a_nexts_done_batch(discriminator_batch_size, s_, a_, s_prime_, done_mask_)
-            expert_s,expert_a,expert_next_s,expert_done = self.data.choose_s_a_nexts_done_batch(discriminator_batch_size, self.expert_states, self.expert_actions, self.expert_next_states, self.expert_dones) 
+            agent_s,agent_a,agent_next_s,agent_done_mask = make_one_mini_batch(discriminator_batch_size, states, actions, next_states, done_masks)
+            expert_s,expert_a,expert_next_s,expert_done = make_one_mini_batch(discriminator_batch_size, self.expert_states, self.expert_actions, self.expert_next_states, self.expert_dones) 
             expert_done_mask = (1 - expert_done.float())
             
             expert_s = np.clip((expert_s - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5).float()
@@ -76,7 +77,7 @@ class PPO(nn.Module):
                                           writer, discriminator, n_epi, agent_s, agent_a, agent_next_s,\
                                           agent_prob, agent_done_mask, expert_s, expert_a, expert_next_s, expert_prob, expert_done_mask)
 
-        self.train_ppo(writer,n_epi,s_, a_, r_, s_prime_, done_mask_, old_log_prob_)
+        self.train_ppo(writer,n_epi,states, actions, rewards, next_states, done_masks, old_log_probs)
         
     def train_discriminator(self, writer, discriminator, n_epi, agent_s, agent_a, expert_s, expert_a):
         discriminator.train_discriminator(writer, n_epi,agent_s,agent_a,expert_s,expert_a)
@@ -85,46 +86,32 @@ class PPO(nn.Module):
                             agent_next_s, agent_prob, agent_done_mask, expert_s, expert_a, expert_next_s, expert_prob,expert_done_mask):
         discriminator.train_discriminator(writer, n_epi, agent_s, agent_a, agent_next_s, agent_prob, agent_done_mask, expert_s, expert_a, expert_next_s, expert_prob, expert_done_mask)
         
-    def train_ppo(self, writer, n_epi, s_, a_, r_, s_prime_, done_mask_, old_log_prob_):
-        old_value_ = self.v(s_).detach()
-        td_target = r_ + self.args.gamma * self.v(s_prime_) * done_mask_
-        delta = td_target - old_value_
-        delta = delta.detach().cpu().numpy()
-        advantage_lst = []
-        advantage = 0.0
+    def train_ppo(self, writer,n_epi,states, actions, rewards, next_states, done_masks, old_log_probs):
+        old_values, advantages = self.get_gae(states, rewards, next_states, done_masks)
+        returns = advantages + old_values
+        advantages = (advantages - advantages.mean())/(advantages.std()+1e-3)
         
-        for idx in reversed(range(len(delta))):
-            if done_mask_[idx] == 0:
-                advantage = 0.0
-            advantage = self.args.gamma * self.args.lambda_ * advantage + delta[idx][0]
-            advantage_lst.append([advantage])
-        advantage_lst.reverse()
-        advantage_ = torch.tensor(advantage_lst, dtype=torch.float).to(self.device)
-        returns = advantage_ + self.v(s_)
-        advantage_ = (advantage_ - advantage_.mean())/(advantage_.std()+1e-3)
         for i in range(self.args.train_epoch):
-            for s,a,r,s_prime,done_mask,old_log_prob,advantage,return_,old_value in self.data.choose_mini_batch(\
-                                                                              self.args.batch_size,s_, a_, r_, s_prime_, done_mask_, old_log_prob_,advantage_,returns,old_value_): 
-                curr_mu,curr_sigma = self.pi(s)
-                value = self.v(s).float()
+            for state,action,old_log_prob,advantage,return_,old_value \
+            in make_mini_batch(self.args.batch_size, states, actions, \
+                                           old_log_probs,advantages,returns,old_values): 
+                curr_mu,curr_sigma = self.get_action(state)
+                value = self.v(state).float()
                 curr_dist = torch.distributions.Normal(curr_mu,curr_sigma)
                 entropy = curr_dist.entropy() * self.args.entropy_coef
-                curr_prob = curr_dist.log_prob(a).sum(-1,keepdim = True)
-                
-                ratio = torch.exp(curr_prob - old_log_prob.detach())
+                curr_log_prob = curr_dist.log_prob(action).sum(1,keepdim = True)
+
+                #policy clipping
+                ratio = torch.exp(curr_log_prob - old_log_prob.detach())
                 surr1 = ratio * advantage
                 surr2 = torch.clamp(ratio, 1-self.args.max_clip, 1+self.args.max_clip) * advantage
-                
                 actor_loss = (-torch.min(surr1, surr2) - entropy).mean() 
                 
+                #value clipping (PPO2 technic)
                 old_value_clipped = old_value + (value - old_value).clamp(-self.args.max_clip,self.args.max_clip)
                 value_loss = (value - return_.detach().float()).pow(2)
                 value_loss_clipped = (old_value_clipped - return_.detach().float()).pow(2)
-                
                 critic_loss = 0.5 * self.args.critic_coef * torch.max(value_loss,value_loss_clipped).mean()
-                if self.writer != None:
-                    self.writer.add_scalar("loss/actor_loss", actor_loss.item(), n_epi)
-                    self.writer.add_scalar("loss/critic_loss", critic_loss.item(), n_epi)
                 
                 self.actor_optimizer.zero_grad()
                 actor_loss.backward()
@@ -136,3 +123,22 @@ class PPO(nn.Module):
                 nn.utils.clip_grad_norm_(self.critic.parameters(), self.args.max_grad_norm)
                 self.critic_optimizer.step()
                 
+                if writer != None:
+                    writer.add_scalar("loss/actor_loss", actor_loss.item(), n_epi)
+                    writer.add_scalar("loss/critic_loss", critic_loss.item(), n_epi)
+
+    def get_gae(self, states, rewards, next_states, done_masks):
+        values = self.v(states).detach()
+        td_target = rewards + self.args.gamma * self.v(next_states) * done_masks
+        delta = td_target - values
+        delta = delta.detach().cpu().numpy()
+        advantage_lst = []
+        advantage = 0.0
+        for idx in reversed(range(len(delta))):
+            if done_masks[idx] == 0:
+                advantage = 0.0
+            advantage = self.args.gamma * self.args.lambda_ * advantage + delta[idx][0]
+            advantage_lst.append([advantage])
+        advantage_lst.reverse()
+        advantages = torch.tensor(advantage_lst, dtype=torch.float).to(self.device)
+        return values, advantages
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discriminators.vail     import VAIL
 from discriminators.airl     import AIRL
 from discriminators.vairl    import VAIRL
 from discriminators.eairl    import EAIRL
-from utils.utils             import RunningMeanStd, Dict
+from utils.utils             import RunningMeanStd, Dict, make_transition
 
 import os
 import gym
@@ -33,7 +33,7 @@ parser.add_argument('--epoch', type=int, default=1001, help='number of epochs, (
 parser.add_argument("--agent", type=str, default = 'ppo', help = 'actor training algorithm(default: ppo)')
 parser.add_argument("--discriminator", type=str, default = 'gail', help = 'discriminator training algorithm(default: gail)')
 parser.add_argument("--save_interval", type=int, default = 100, help = 'save interval')
-parser.add_argument("--print_interval", type=int, default = 20, help = 'print interval')
+parser.add_argument("--print_interval", type=int, default = 1, help = 'print interval')
 parser.add_argument('--tensorboard', type=bool, default=True, help='use_tensorboard, (default: True)')
 
 args = parser.parse_args()
@@ -80,42 +80,48 @@ state_lst = []
 
 score = 0.0
 
-s_ = (env.reset())
-s = np.clip((s_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
+state_ = (env.reset())
+state = np.clip((state_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
 for n_epi in range(args.epoch):
     for t in range(agent_args.traj_length):
         if args.render:    
             env.render()
-        state_lst.append(s_)
-        mu,sigma = agent.pi(torch.from_numpy(s).float().to(device))
+        state_lst.append(state_)
+        mu,sigma = agent.get_action(torch.from_numpy(state).float().to(device))
         dist = torch.distributions.Normal(mu,sigma[0])
 
         action = dist.sample()
         log_prob = dist.log_prob(action).sum(-1,keepdim = True)
-        s_prime_, r, done, info = env.step(action.unsqueeze(0).cpu().numpy())
-        s_prime = np.clip((s_prime_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
+        next_state_, reward, done, info = env.step(action.unsqueeze(0).cpu().numpy())
+        next_state = np.clip((next_state_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
         if discriminator_args.is_airl:
             reward = discriminator.get_reward(\
                         log_prob,
-                        torch.tensor(s).unsqueeze(0).float().to(device),action.unsqueeze(0),\
-                        torch.tensor(s_prime).unsqueeze(0).float().to(device),\
+                        torch.tensor(state).unsqueeze(0).float().to(device),action.unsqueeze(0),\
+                        torch.tensor(next_state).unsqueeze(0).float().to(device),\
                                               torch.tensor(done).unsqueeze(0)\
                                              ).item()
         else:
-            reward = discriminator.get_reward(torch.tensor(s).unsqueeze(0).float().to(device),action.unsqueeze(0)).item()
-        agent.put_data((s, action, reward/10., s_prime, \
-                        log_prob.detach().cpu().numpy(), done))
-        score += r 
+            reward = discriminator.get_reward(torch.tensor(state).unsqueeze(0).float().to(device),action.unsqueeze(0)).item()
+        transition = make_transition(state,\
+                                     action,\
+                                     np.array([reward/10.0]),\
+                                     next_state,\
+                                     np.array([done]),\
+                                     log_prob.detach().cpu().numpy()\
+                                    )
+        agent.put_data(transition) 
+        score += reward
         if done:
-            s_ = (env.reset())
-            s = np.clip((s_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
+            state_ = (env.reset())
+            state = np.clip((state_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
             score_lst.append(score)
             if writer != None:
                 writer.add_scalar("score", score, n_epi)
             score = 0
         else:
-            s = s_prime
-            s_ = s_prime_
+            state = next_state
+            state_ = next_state_
     agent.train(writer, discriminator, discriminator_args.batch_size, state_rms, n_epi, airl = discriminator_args.is_airl)
     state_rms.update(np.vstack(state_lst))
     state_lst = []
